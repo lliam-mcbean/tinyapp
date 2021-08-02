d8e708e5ec51d879dad42b784699fde34cd64828
Lliam McBean <lliam.mcbean@gmail.com>
2021-08-02 22:39:37 +0000

    added the random number function and took the longURL in our /POST request

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 const urlDatabase = {
@@ -9,14 +11,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+    let randomString = ''
+    for (let i = 0; i < 6; i++) {
+        let randomNumber = Math.round((Math.random() * 36))
+        randomString += randomNumber.toString()
+    }
+    return randomString
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
