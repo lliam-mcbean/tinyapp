51464848a9e300bf5cd070f89ea6bd10b8d6a4a6
Lliam McBean <lliam.mcbean@gmail.com>
2021-08-03 23:24:04 +0000

    adding login info

--- express_server.js
+++ express_server.js
@@ -12,9 +12,9 @@ const urlDatabase = {
 };
 
 const generateRandomString = () => {
-    let randomString = ''
-    randomString += Math.random().toString(20).substr(2, 6)
-    return randomString
+  let randomString = ''
+  randomString += Math.random().toString(20).substr(2, 6)
+  return randomString
 }
 
 app.get("/", (req, res) => {
@@ -22,16 +22,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { 
+    urls: urlDatabase,
+    // username: req.cookies["username"] 
+  }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"] 
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -40,8 +50,8 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL]
-    res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
@@ -58,6 +68,11 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL
   res.redirect(`/urls/${req.params.shortURL}`)
+})
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
 })
 
 app.listen(PORT, () => {
