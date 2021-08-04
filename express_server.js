31088bba8de4c78b0b0b895b5f77c521291aa98e
Lliam McBean <lliam.mcbean@gmail.com>
2021-08-04 20:36:57 +0000

    made the login logic for user emails

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const cookies = require("cookie-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
@@ -12,12 +11,34 @@ app.set('view engine', 'ejs')
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+}
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 const generateRandomString = () => {
   let randomString = ''
   randomString += Math.random().toString(20).substr(2, 6)
   return randomString
+}
+
+const emailSearch = (users, email) => {
+  for (const people in users) {
+    if (email === users[people].email) {
+      return false
+    }
+  }
+  return true
 }
 
 app.get("/", (req, res) => {
@@ -26,7 +47,8 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    username: req.cookies['username'], 
+    username: users,
+    user_id: req.cookies['user_id'], 
     urls: urlDatabase
   }
   res.render("urls_index", templateVars);
@@ -34,16 +56,26 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies['username']
+    username: users,
+    user_id: req.cookies['user_id']
   }
   res.render("urls_new", templateVars);
 });
 
+app.get('/register', (req, res) => {
+  const templateVars = {
+    username: users,
+    user_id: req.cookies['user_id']
+  }
+  res.render("urls_register", templateVars);
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
+    username: users,
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies['username']
+    user_id: req.cookies['user_id']
   };
   res.render("urls_show", templateVars);
 });
@@ -57,9 +89,10 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.get('/testing', (req, res) => {
+app.get('*', (req, res) => {
   const templateVars = {
-    username: req.cookies['username']
+    username: users,
+    user_id: req.cookies['user_id']
   }
   res.render('urls_404', templateVars)
 })
@@ -88,6 +121,27 @@ app.post('/login', (req, res) => {
 app.post('/logout', (req, res) => {
   res.clearCookie('username')
   res.redirect('/urls')
+})
+
+app.post('/register', (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    res.statusCode = 400
+    res.end()
+  } else if (!emailSearch(users, req.body.email)) {
+    res.statusCode = 400
+    res.end()
+  } else {
+    const newUserId = generateRandomString()
+    users[newUserId] = {
+      id: newUserId,
+      email: req.body.email,
+      password: req.body.password
+    }
+    console.log(users)
+    res.cookie('user_id', newUserId)
+    res.redirect('/urls')
+  }
+  console.log(users)
 })
 
 app.listen(PORT, () => {
